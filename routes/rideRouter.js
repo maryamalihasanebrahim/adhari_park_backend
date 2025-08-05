8093cc8b19fc101e1cb6a32012811d70adcb697d
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-08-05 12:43:23 +0300

    get ride routes

--- routes/rideRouter.js
+++ routes/rideRouter.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/rideController')
 
 router.post('/new', controller.CreateRide)
 router.get('/', controller.GetRides)
+router.get('/:id', controller.GetRide)
 
 
 module.exports = router
